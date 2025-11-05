5607be5eca749aad804ce8d6eddd4be0ea9b57dc
Farzam Asadian <farzamasadian@gmail.com>
2025-11-05 10:17:02 +0330

    names_list added and get method is also added for the name list

--- src/main.py
+++ src/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+names_list=[
+    {"id":1, "name":"a"},
+    {"id":2, "name":"b"},
+    {"id":3, "name":"c"},
+    {"id":4, "name":"d"},
+]
 
 @app.get("/")
 def hello_world():
     return {"message": "Hello World"}
 
+@app.get("/names")
+def get_names_list():
+    return names_list
+
+# @app.get("/names")
+# def get_names_list():
+#     return names_list
