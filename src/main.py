e5d0f0cae2a8852063b6bd237e44e91260bba988
Farzam Asadian <farzamasadian@gmail.com>
2025-11-08 12:33:17 +0330

    added some changes in query parameters, path parameters and ...

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Query, status, HTTPException
+from fastapi import FastAPI, Query, status, HTTPException, Path, Form, Body
+from fastapi.responses import JSONResponse
 from random import randint
 from typing import Annotated
-status.HTTP_404_NOT_FOUND
 app = FastAPI()
 
 names_list=[
@@ -16,23 +16,23 @@ names_list=[
 ]
 
 @app.get("/")
-def hello_world():
-    return {"message": "Hello World"}
+def root():
+    return JSONResponse(content={"message": "Hello World"}, status_code=status.HTTP_202_ACCEPTED)
 
 @app.get("/names")
-def get_names_list(q : Annotated[str | None, Query(max_length=50)]=None):
+def get_names_list(q : Annotated[str | None, Query(alias="search",description="item will be searched with the title you provided", example="a", max_length=50)]=None):
     if q:
         return [item for item in names_list if item["name"] == q]
     return names_list
 
 @app.post("/names", status_code=status.HTTP_201_CREATED)
-def create_name(name:str):
+def create_name(name:str = Body(embed=True)):
     name_obj = {"id":randint(5,100), "name":name}
     names_list.append(name_obj)
     return name_obj
 
 @app.get("/names/{name_id}")
-def get_names_detail(name_id:int):
+def get_names_detail(name_id:int = Path(alias="object id", title="object id", description="the id of the name in names_list")):
     for item in names_list:
         if item["id"] == name_id:
             return item
@@ -51,5 +51,5 @@ def delete_name(name_id:int):
     for item in names_list:
         if item["id"] == name_id:
             names_list.remove(item)
-            return {"detail": "Object Removed Successfuly" }
+            return JSONResponse(content={"detail": "Object Removed Successfuly" }, status_code=status.HTTP_200_OK)
     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail="object not found")
